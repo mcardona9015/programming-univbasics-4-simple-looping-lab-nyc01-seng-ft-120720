39773d65d140e67ef5dcd43aa231f4e69cf01e15
Michael Cardona <mcardona9015@gmail.com>
2020-11-20 19:04:01 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,12 @@
 # Write your methods here
+def loop_message_five_times(message)
+  5.times do
+    puts message
+  end
+end
+
+def loop_message_n_times(message, n)
+  n.times do
+    puts message
+  end
+end
